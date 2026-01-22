b116543d75978892bf0261474ad7bfdea4a58909
ammar <ammarshaban5@gmail.com>
2026-01-22 09:23:45 +0300

    add user table

--- src/components/Users/UsersTable.tsx
+++ src/components/Users/UsersTable.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+// import { useEffect, useState } from "react";
 
 import type { Users } from "./column";
 import { columns } from "./column";
 import { DataTable } from "./data-table";
 
-export default function UsersTable() {
-  const [data, setData] = useState<Users[]>([]);
+const data: Users[] = [
+  { id: "1", name: "John Doe", email: "john@example.com" },
+  { id: "2", name: "Jane Smith", email: "jane@example.com" },
+  { id: "3", name: "Bob Johnson", email: "bob@example.com" },
+  { id: "4", name: "Alice Brown", email: "alice@example.com" },
+  { id: "5", name: "ammar shaban", email: "ammar@example.com" },
+  { id: "6", name: "maen haddad", email: "maen@example.com" },
+  { id: "7", name: "ahmed Brown", email: "ahmed@example.com" },
+  { id: "8", name: "khalid tamemy", email: "khalid@example.com" },
+];
 
+export default function UsersTable() {
+  // const [data, setData] = useState<Users[]>([]);
   // const [loading, setLoading] = useState(false);
   // const [error, setError] = useState();
   // useEffect(() => {
--- src/components/Users/column.tsx
+++ src/components/Users/column.tsx
@@ -1,7 +1,17 @@
 import type { ColumnDef } from "@tanstack/react-table";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export type Users = {
   id: string;
   name: string;
@@ -20,5 +30,33 @@ export const columns: ColumnDef<Users>[] = [
   {
     accessorKey: "email",
     header: "Email",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy user ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View user details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
